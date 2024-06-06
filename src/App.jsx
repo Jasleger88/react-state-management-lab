e9b4abd6ad765e4edfa877b0153cc1dfecfa4dea
Jasmine Leger <Jasmineleger21@gmail.com>
2024-06-06 06:26:57 -0400

    react state management lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 // src/App.jsx
+import React from "react"
+import ZombieFighters from "./components/zombieFighters";
+import {initialZombies} from "./components/zombieFighters";
+
+
+
+
 
 const App = () => {
 
   return (
-    <h1>Hello world!</h1>
+
+    <div className="App">
+      <h1>Zombie Fighters In Motion </h1>
+      <section>
+        {initialZombies.map((zombiesFighters, index)=> (
+          <zombiesFighterss key= {index} zombieFighters= {zombiesFighter}/>
+        ))}
+      </section>
+    </div>
   );
 }
+
+
+
 
 export default App
